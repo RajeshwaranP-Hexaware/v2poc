b2173b3a08cd1c552991a4be59cd94902990e593
Rajeshwaran <rajeshfederer2@gmail.com>
2018-08-13 12:16:00 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+const port = process.env.PORT || 3001;
+const server = app.listen(port, function () {
+    console.log("Application started listening port " + port);
+});
+
+app.post("/test", function (req, res) {
+    console.log("inside test", req.body);
+    res.json({
+        "fulfillmentText": "This is a text response",
+        "fulfillmentMessages": [
+            {
+            "card": {
+                    "title": "card title",
+                    "subtitle": "card text",
+                    "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+                    "buttons": [
+                    {
+                        "text": "button text",
+                        "postback": "https://assistant.google.com/"
+                    }
+                    ]
+                }
+            }
+        ],
+    });
+});
